06d3fb3757853f5df1e6f794b1b7828fbfd68311
AlexandraKovalenko <31831658+AlexandraKovalenko@users.noreply.github.com>
2017-11-01 00:16:55 +0300

    Delete task7.py

--- Homework 3/task7.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        N = int(input("Enter int number of minutes\n"))
-        if N >= 0:
-            break
-    except ValueError:
-        print ("\nWhat you entered is not a number")
-  
-print((N % 1440) // 60, ":", (N % 1440) % 60)
